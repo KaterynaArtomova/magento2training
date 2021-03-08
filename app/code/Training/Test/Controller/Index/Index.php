9bf79faa2c1dfd1bd3a0586d80002ee90386fdae
Katerina Artyomova <katerina.artyomova@skynix.co>
2021-03-08 16:23:46 +0200

    homework task 2.4.3

--- app/code/Training/Test/Controller/Index/Index.php
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -4,11 +4,25 @@
 namespace Training\Test\Controller\Index;
 
 
-class Index extends \Magento\Framework\App\Action\Action
+class Index implements \Magento\Framework\App\ActionInterface
 {
 
+    private $resultRawFactory;
+    private $resultFactory;
+
+    public function __construct(
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+        \Magento\Framework\Controller\ResultFactory $resultFactory
+    ) {
+        $this->resultRawFactory = $resultRawFactory;
+        $this->resultFactory = $resultFactory;
+    }
     public function execute()
     {
-        $this->getResponse()->appendBody('simple text');
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents('simple text');
+        $resultRaw->setHeader('Content-Type', 'text/txt', true);
+        return $resultRaw;
     }
 }
+
