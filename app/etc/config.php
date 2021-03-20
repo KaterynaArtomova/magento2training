43ca8b9f4d0d4b384be2c5bacf20dda6d025093b
Katerina Artyomova <katerina.artyomova@skynix.co>
2021-03-20 08:33:18 +0200

    homework task 4.3

--- app/etc/config.php
+++ app/etc/config.php
@@ -315,6 +315,7 @@ return [
         'Training_CheckPluginFunctionality' => 1,
         'Training_Dependency2' => 1,
         'Training_Dependency' => 1,
+        'Training_Js' => 1,
         'Training_Render' => 1,
         'Training_Test' => 1,
         'Training_AdditionalTestOM' => 1,
